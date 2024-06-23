cc554663b95f59a98d378a64833f8dacc56c9d55
Md Tarek Miah <140520331+Tarek00T@users.noreply.github.com>
2024-06-23 23:32:00 +0600

    Add files via upload

---
+++ str_sorting.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std ;
+void solve()
+{
+    string s;
+    cin >>s;
+    sort(s.begin(),s.end());
+    cout<<s<<endl;
+}
+int main ()
+{
+    int t=1;
+    //cin >>t;
+    while(t--)
+    {
+        solve();
+    }
+}
+
+
