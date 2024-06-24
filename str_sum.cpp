c33a4cd660d2b4027067a10dd7434c162b6392f4
Md Tarek Miah <140520331+Tarek00T@users.noreply.github.com>
2024-06-24 09:30:19 +0600

    Add files via upload

---
+++ str_sum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std ;
+void solve()
+{
+    string s1,s2,s;
+    cin >>s1>>s2;
+    s=s1 + s2;
+    cout<<s<<" "<<endl;
+
+}
+int main ()
+{
+    int t=1;
+    //cin >>t;
+    while(t--)
+    {
+        solve();
+    }
+}
+
+
+
