2bb2994ab2afc52677eb3efe9ebcb890d2ff6c87
Md Tarek Miah <140520331+Tarek00T@users.noreply.github.com>
2024-06-24 09:22:20 +0600

    Add files via upload

---
+++ str_reverse.cpp
@@ -0,0 +1,21 @@
+
+#include<bits/stdc++.h>
+using namespace std ;
+void solve()
+{
+    string s;
+    cin >>s;
+    reverse(s.begin(),s.end());
+    cout<<s<<endl;
+}
+int main ()
+{
+    int t=1;
+    //cin >>t;
+    while(t--)
+    {
+        solve();
+    }
+}
+
+
